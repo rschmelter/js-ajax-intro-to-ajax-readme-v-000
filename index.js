272dc16c419964a51ec491d15c838748f9793079
Ryan Schmelter <rschmelter33@gmail.com>
2018-09-06 00:21:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ function getRepositories() {
   req.send();
 }
 function showRepositories() {
-  console.log(this.responseText);
-  let repoList = '<ul>';
-for (var i = 0; i < this.responseText.length; i++) {
-  repoList += '<li>' + this.responseText[i]['name'] + '</li>';
-}
-repoList += '</ul>';
-document.getElementById('repositories').innerHTML = repoList;
+  var repos = JSON.parse(this.responseText);
+  console.log(repos);
+  const repoList = `<ul>${repos
+    .map(r => '<li>' + r.name + '</li>')
+    .join('')}</ul>`;
+  document.getElementById('repositories').innerHTML = repoList;
 
 }
